2ab997dadd3d7ad9f5c8f600db4f54ccf1b2fdbb
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-02-12 13:19:58 +0100

    step 1

--- starters/php/tests/unit/BowlingScorerTest.php
+++ starters/php/tests/unit/BowlingScorerTest.php
@@ -5,4 +5,11 @@ use PHPUnit\Framework\TestCase;
 final class BowlingScorerTest extends TestCase
 {
 
+    public function testShouldBeAbleToScore(): void
+    {
+        $bowlingScorer = new BowlingScorer();
+
+        $bowlingScorer->roll(0);
+
+    }
 }
