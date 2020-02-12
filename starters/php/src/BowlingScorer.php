e5fbcc958758b13d9ad4f93cdcaad75e0cb80502
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-02-12 16:28:37 +0100

    next step (refactor)

--- starters/php/src/BowlingScorer.php
+++ starters/php/src/BowlingScorer.php
@@ -10,25 +10,21 @@ class BowlingScorer
 
     public function roll(int $roll)
     {
-        if ($this->nextToLastRoll == 10) {
+        if ($this->shouldAddStrikeScore()) {
             $this->score += 10 + $this->lastRoll + $roll;
-            $this->nextToLastRoll = -1;
-        } else if ($this->nextToLastRoll + $this->lastRoll == 10) {
+        } else if ($this->shouldAddSpareScore()) {
             $this->score += 10 + $roll;
             $this->nextToLastRoll = -1;
             $this->lastRoll = -1;
         }
 
         if ($this->lastRoll >= 0) {
-            if ($this->lastRoll == 10) {
+            if ($this->countingStrike() || $this->countingSpare($roll)) {
                 $this->nextToLastRoll = $this->lastRoll;
                 $this->lastRoll = $roll;
-            } else if ($this->lastRoll + $roll != 10) {
+            } else {
                 $this->score += $this->lastRoll + $roll;
                 $this->lastRoll = -1;
-            } else {
-                $this->nextToLastRoll = $this->lastRoll;
-                $this->lastRoll = $roll;
             }
         } else {
             $this->lastRoll = $roll;
@@ -38,5 +34,38 @@ class BowlingScorer
     public function score(): int
     {
         return $this->score;
+    }
+
+    /**
+     * @return bool
+     */
+    public function shouldAddStrikeScore(): bool
+    {
+        return $this->nextToLastRoll == 10;
+    }
+
+    /**
+     * @return bool
+     */
+    public function shouldAddSpareScore(): bool
+    {
+        return $this->nextToLastRoll + $this->lastRoll == 10;
+    }
+
+    /**
+     * @return bool
+     */
+    public function countingStrike(): bool
+    {
+        return $this->lastRoll == 10;
+    }
+
+    /**
+     * @param int $roll
+     * @return bool
+     */
+    public function countingSpare(int $roll): bool
+    {
+        return $this->lastRoll + $roll == 10;
     }
 }
