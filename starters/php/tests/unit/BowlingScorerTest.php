1fb6d510232a1d2e1100a4c8efac9b5ca533bcfa
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-02-12 16:09:11 +0100

    next step (with slight refactor added)

--- starters/php/tests/unit/BowlingScorerTest.php
+++ starters/php/tests/unit/BowlingScorerTest.php
@@ -17,57 +17,74 @@ final class BowlingScorerTest extends TestCase
     {
         $this->roll(0);
 
-        $this->assertEquals(0, $this->bowlingScorer->score());
+        $this->assertScore(0);
     }
 
     public function testShouldBeAbleToRollAndSeeTotalScore(): void
     {
         $this->roll(2, 3);
 
-        $this->assertEquals(5, $this->bowlingScorer->score());
+        $this->assertScore(5);
     }
 
     public function testShouldHaveACompleteZeroGame(): void
     {
         $this->roll(0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
 
-        $this->assertEquals(0, $this->bowlingScorer->score());
+        $this->assertScore(0);
     }
 
     public function testShouldAccountScoreForSpares(): void
     {
         $this->roll(5, 5);
-        $this->assertEquals(0, $this->bowlingScorer->score());
+        $this->assertScore(0);
         $this->roll(2);
-        $this->assertEquals(12, $this->bowlingScorer->score()); //should account previous frame, but not yet the current one
+        $this->assertScore(12); //should account previous frame, but not yet the current one
         $this->roll(7);
-        $this->assertEquals(21, $this->bowlingScorer->score()); //should account previous and current frame
+        $this->assertScore(21); //should account previous and current frame
     }
 
     public function testShouldAccountForTwoSparesInARow(): void
     {
         $this->roll(5,5);
-        $this->assertEquals(0, $this->bowlingScorer->score());
+        $this->assertScore(0);
         $this->roll(2);
-        $this->assertEquals(12, $this->bowlingScorer->score()); //add up spare frame score
+        $this->assertScore(12); //add up spare frame score
         $this->roll(8);
-        $this->assertEquals(12, $this->bowlingScorer->score()); //another spare frame, not account this one yet...
+        $this->assertScore(12); //another spare frame, not account this one yet...
         $this->roll(3);
-        $this->assertEquals(25, $this->bowlingScorer->score()); //add up a second spare frame score
+        $this->assertScore(25); //add up a second spare frame score
     }
 
     public function testShouldHaveFullGameWithSpares(): void
     {
         $this->roll(5,5, 2,1, 3,5, 6,2, 0,2, 1,2, 0,0, 4,6, 3,2, 4,4);
-        $this->assertEquals(62, $this->bowlingScorer->score());
+        $this->assertScore(62);
     }
 
+    public function testShouldHaveFullSpareGame(): void
+    {
+        $this->roll(5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5);
+        $this->assertScore(150);
+    }
 
+    public function testShouldAccountForStrikes(): void
+    {
+        $this->roll(10);
+        $this->assertScore(0);
+        $this->roll(2, 3);
+        $this->assertScore(20);
+    }
 
     private function roll(...$rolls): void
     {
         foreach ($rolls as $roll) {
             $this->bowlingScorer->roll($roll);
         }
+    }
+
+    public function assertScore(int $expected): void
+    {
+        $this->assertEquals($expected, $this->bowlingScorer->score());
     }
 }
