d68ba9e1aa1100e5b2b3bc5f888cb23c8ee16c06
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-02-12 15:52:08 +0100

    next step

--- starters/php/src/BowlingScorer.php
+++ starters/php/src/BowlingScorer.php
@@ -6,13 +6,23 @@ class BowlingScorer
 
     private int $score = 0;
 
-    private int $lastRoll = -1;
+    private int $lastRoll = -1, $nextToLastRoll = -1;
 
     public function roll(int $roll)
     {
+        if ($this->nextToLastRoll + $this->lastRoll == 10) {
+            $this->score += 10 + $roll;
+            $this->nextToLastRoll = -1;
+            $this->lastRoll = -1;
+
+        }
         if ($this->lastRoll >= 0) {
             if ($this->lastRoll + $roll != 10) {
                 $this->score += $this->lastRoll + $roll;
+                $this->lastRoll = -1;
+            } else {
+                $this->nextToLastRoll = $this->lastRoll;
+                $this->lastRoll = $roll;
             }
         } else {
             $this->lastRoll = $roll;
