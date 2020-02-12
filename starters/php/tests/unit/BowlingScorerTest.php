2992d6d64ca0b0a5d0e208331aa90d4067bf3e93
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-02-12 13:25:49 +0100

    step 7

--- starters/php/tests/unit/BowlingScorerTest.php
+++ starters/php/tests/unit/BowlingScorerTest.php
@@ -14,4 +14,15 @@ final class BowlingScorerTest extends TestCase
         $this->assertEquals(0, $bowlingScorer->score());
 
     }
+
+    public function testShouldBeAbleToRollAndSeeTotalScore(): void
+    {
+        $bowlingScorer = new BowlingScorer();
+
+        $bowlingScorer->roll(2);
+        $bowlingScorer->roll(3);
+
+        $this->assertEquals(5, $bowlingScorer->score());
+
+    }
 }
