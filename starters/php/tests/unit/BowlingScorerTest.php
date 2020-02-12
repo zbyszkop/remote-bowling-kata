6d3d2ce520a289ef1022f883472939780d873e31
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-02-12 13:25:00 +0100

    step 6 (refactor - one test included another one)

--- starters/php/tests/unit/BowlingScorerTest.php
+++ starters/php/tests/unit/BowlingScorerTest.php
@@ -5,15 +5,6 @@ use PHPUnit\Framework\TestCase;
 final class BowlingScorerTest extends TestCase
 {
 
-    public function testShouldBeAbleToScore(): void
-    {
-        $bowlingScorer = new BowlingScorer();
-
-        $bowlingScorer->roll(0);
-
-        $this->assertTrue(true); //fake assertion so test passes
-    }
-
     public function testShouldBeAbleToScoreAndSeeScore(): void
     {
         $bowlingScorer = new BowlingScorer();
